1f80d803270b907dd2421acc40df3971dd1dfbae
sk-upadhyay <shivamws2003@gmail.com>
2025-07-13 11:55:24 +0530

    UpdatedAgain1

--- Frontend/src/Pages/RetailerDashboard.jsx
+++ Frontend/src/Pages/RetailerDashboard.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import WalmartInventoryDashboard from "../retailerComponents/components/retailer-inventory-component";
 function RetailerDashboard({ user, onLogout }) {
-
+ 
   return (
     <div className="min-h-screen bg-gray-50 p-4">
       <div className="max-w-7xl mx-auto">
@@ -12,18 +11,13 @@ function RetailerDashboard({ user, onLogout }) {
             <p className="text-gray-600">Welcome, {user.name}!</p>
           </div>
           <button 
-            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
-          >
-          <Link to="/scanner">Go to Scanner</Link>
-          </button>
-          <button 
             onClick={onLogout}
             className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition"
           >
             Logout
           </button>
         </header>
-        
+        <WalmartInventoryDashboard />
       </div>
     </div>
   );
